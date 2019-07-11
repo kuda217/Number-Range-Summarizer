5be9a2a45170c36f90799782102d15d3c2762420
k0da_Machina <kuda.mawonde@gmail.com>
2019-07-11 10:15:14 +0200

    Create NumberRangeSummarizer.java

---
+++ NumberRangeSummarizer.java
@@ -0,0 +1,62 @@
+public class NumberRangeSummarizer implements Collector<Integer, List<String>, List<String>>{
+
+    private int last = 0;
+    private LinkedList<Integer> intermediate = new LinkedList<>();
+
+    @Override
+    public Supplier<List<String>> supplier(){
+        return ArrayList::new;
+    }
+
+    @Override
+    public BiConsumer<List<String>, Integer> accumulator(){
+        return ( finalList, current ) -> {
+            if( current - last == 1 ){ // check if adjacent to last value
+                intermediate.add(current);
+            } else{
+                if( intermediate.size() > 2 ){
+                    finalList.add(intermediate.getFirst() + "-" + intermediate.getLast()); // add new range
+                } else{
+                    addLeftOverValues(finalList);
+                }
+                intermediate.clear();
+                intermediate.add(current);
+            }
+            last = current;
+        };
+    }
+
+    @Override
+    public BinaryOperator<List<String>> combiner(){
+        return (list, list2) -> {
+            list.addAll(list2);
+            return list;
+        };
+    }
+
+    @Override
+    public Function<List<String>, List<String>> finisher(){
+        return ( finalList ) -> {
+            if( !intermediate.isEmpty() ){
+                addLeftOverValues(finalList);
+            }
+            return finalList;
+        };
+    }
+
+    @Override
+    public Set<Characteristics> characteristics(){
+        return EnumSet.noneOf(Characteristics.class);
+    }
+
+    private void addLeftOverValues( List<String> list ){
+        list.addAll(
+            intermediate.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList())
+       );
+    }
+}
+
+List<Integer> list = Arrays.asList(1,3,6,7,8,12,13,14,15,21,22,23,24,31);
+System.out.println(list.stream().collect(new NumberRangeSummarizer()));
